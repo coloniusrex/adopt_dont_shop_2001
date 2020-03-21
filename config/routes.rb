6ef2fb2f33eee2409f8fb674bea50837a2868b4c
Colin Alexander <colintalex@gmail.com>
2020-03-20 23:22:18 -0600

    Creates routes for delete and edit functionality

--- config/routes.rb
+++ config/routes.rb
@@ -5,5 +5,7 @@ Rails.application.routes.draw do
   get '/shelters/new', to: 'shelters#new'
   post '/shelters', to: 'shelters#create'
   get '/shelters/:id', to: 'shelters#show'
-
+  delete '/shelters/:id', to: 'shelters#destroy'
+  get '/shelters/:id/edit', to: 'shelters#edit'
+  patch '/shelters/:id', to: 'shelters#update'
 end
