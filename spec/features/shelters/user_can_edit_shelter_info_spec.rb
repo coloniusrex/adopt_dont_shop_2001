447b8f5bf6beb20d9630f933b3ffbdf7e77babc4
Colin Alexander <colintalex@gmail.com>
2020-03-20 23:23:43 -0600

    Creates test for editing shelter info

---
+++ spec/features/shelters/user_can_edit_shelter_info_spec.rb
@@ -0,0 +1,24 @@
+require 'rails_helper'
+
+RSpec.describe "As a visitor" do
+  it "I can edit a shelter's info" do
+    happy_hills = Shelter.create(name:"Happy Hills",
+                           address: "300 Street")
+    sunny_valley = Shelter.create(name: "Sunny Valley",
+                                address: "400 Street")
+
+    visit "/shelters/#{happy_hills.id}"
+
+    click_on 'Edit'
+
+    expect(current_path).to eq("/shelters/#{happy_hills.id}/edit")
+
+    fill_in 'Name', with: 'Happier Hills'
+    click_on 'Update Shelter'
+
+    expect(current_path).to eq("/shelters/#{happy_hills.id}")
+    expect(page).to have_content('Happier Hills')
+    expect(page).to_not have_content('Happy Hills')
+
+  end
+end
